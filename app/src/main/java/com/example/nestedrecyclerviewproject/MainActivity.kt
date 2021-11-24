5be306aa5b7e1526f83223e77d70274108ed4851
Nikolui333 <semenovnikolu@yandex.ru>
2021-11-24 21:38:20 +0300

    string

--- app/src/main/java/com/example/nestedrecyclerviewproject/MainActivity.kt
+++ app/src/main/java/com/example/nestedrecyclerviewproject/MainActivity.kt
@@ -12,10 +12,10 @@ class MainActivity : AppCompatActivity() {
     private var genresAdapter:GenresAdapter? = null
 
     private var genresMovie:ArrayList<GenresBook>? = null
-    private var actionMovies:ArrayList<CardBook>? = null
-    private var fantasticMovies:ArrayList<CardBook>? = null
-    private var adventureMovies:ArrayList<CardBook>? = null
-    private var thrillerMovies:ArrayList<CardBook>? = null
+    private var programmingBook:ArrayList<CardBook>? = null
+    private var developingBook:ArrayList<CardBook>? = null
+    private var fictionBook:ArrayList<CardBook>? = null
+    private var philosophyBook:ArrayList<CardBook>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,59 +23,59 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         genresMovie = ArrayList<GenresBook>()
-        actionMovies = ArrayList<CardBook>()
-        fantasticMovies = ArrayList<CardBook>()
-        adventureMovies = ArrayList<CardBook>()
-        thrillerMovies = ArrayList<CardBook>()
+        programmingBook = ArrayList<CardBook>()
+        developingBook = ArrayList<CardBook>()
+        fictionBook = ArrayList<CardBook>()
+        philosophyBook = ArrayList<CardBook>()
 
 
         genresMovie?.add(
-            GenresBook(getString(R.string.actionMovies), actionMovies!!)
+            GenresBook(getString(R.string.programming), programmingBook!!)
         )
 
         genresMovie?.add(
-            GenresBook(getString(R.string.fantasticMovies), fantasticMovies!!)
+            GenresBook(getString(R.string.developing_literature), developingBook!!)
         )
 
         genresMovie?.add(
-            GenresBook(getString(R.string.adventureMovies), adventureMovies!!)
+            GenresBook(getString(R.string.fiction), fictionBook!!)
         )
 
         genresMovie?.add(
-            GenresBook(getString(R.string.thrillerMovies), thrillerMovies!!)
+            GenresBook(getString(R.string.philosophy), philosophyBook!!)
         )
 
 
-        actionMovies?.add(CardBook(R.drawable.bad_boys, getString(R.string.badBoys), getString(R.string.badBoysLong),
-            getString(R.string.badBoysActors), getString(R.string.badBoysRating)))
-        adventureMovies?.add(CardBook(R.drawable.avengers, getString(R.string.avengers), getString(R.string.avengersLong),
-            getString(R.string.avengersActors), getString(R.string.avengersRating)))
-        actionMovies?.add(CardBook(R.drawable.transformers, getString(R.string.transformers), getString(R.string.transformersLong),
-            getString(R.string.transformersActors), getString(R.string.transformersRating)))
-        adventureMovies?.add(CardBook(R.drawable.fast, getString(R.string.fast), getString(R.string.fastLong),
-            getString(R.string.fastActors), getString(R.string.fastRating)))
-        actionMovies?.add(CardBook(R.drawable.mk, getString(R.string.mk), getString(R.string.mkLong),
-            getString(R.string.mkActors), getString(R.string.mkRating)))
-        adventureMovies?.add(CardBook(R.drawable.underworld, getString(R.string.underworld), getString(R.string.underworldLong),
-            getString(R.string.underworldActors), getString(R.string.underworldRating)))
+        programmingBook?.add(CardBook(R.drawable.android_professional, getString(R.string.android_professional), getString(R.string.android_professionalLong),
+            getString(R.string.android_professionalAuthor), getString(R.string.android_professionalRating)))
+        fictionBook?.add(CardBook(R.drawable.noise, getString(R.string.noise), getString(R.string.noiseLong),
+            getString(R.string.noiseAuthor), getString(R.string.noiseRating)))
+        programmingBook?.add(CardBook(R.drawable.learning_java, getString(R.string.learning_java), getString(R.string.learning_javaLong),
+            getString(R.string.learning_javaAuthor), getString(R.string.learning_javaRating)))
+        fictionBook?.add(CardBook(R.drawable.onegin, getString(R.string.onegin), getString(R.string.oneginLong),
+            getString(R.string.oneginAuthor), getString(R.string.oneginRating)))
+        programmingBook?.add(CardBook(R.drawable.head_first, getString(R.string.head_first), getString(R.string.head_firstLong),
+            getString(R.string.head_firstAuthor), getString(R.string.head_firstRating)))
+        fictionBook?.add(CardBook(R.drawable.pride_and_prejudice, getString(R.string.pride_and_prejudice), getString(R.string.pride_and_prejudiceLong),
+            getString(R.string.pride_and_prejudiceAuthor), getString(R.string.pride_and_prejudiceRating)))
 
 
-        fantasticMovies?.add(CardBook(R.drawable.avengers, getString(R.string.avengers), getString(R.string.avengersLong),
-            getString(R.string.avengersActors), getString(R.string.avengersRating)))
-        fantasticMovies?.add(CardBook(R.drawable.transformers, getString(R.string.transformers), getString(R.string.transformersLong),
-            getString(R.string.transformersActors), getString(R.string.transformersRating)))
-        fantasticMovies?.add(CardBook(R.drawable.mk, getString(R.string.mk), getString(R.string.mkLong),
-            getString(R.string.mkActors), getString(R.string.mkRating)))
-        fantasticMovies?.add(CardBook(R.drawable.underworld, getString(R.string.underworld), getString(R.string.underworldLong),
-            getString(R.string.underworldActors), getString(R.string.underworldRating)))
+        developingBook?.add(CardBook(R.drawable.instruction_for_the_brain, getString(R.string.instruction_for_the_brain), getString(R.string.instruction_for_the_brainLong),
+            getString(R.string.instruction_for_the_brainAuthor), getString(R.string.instruction_for_the_brainRating)))
+        developingBook?.add(CardBook(R.drawable.jedi_techniques, getString(R.string.jedi_techniques), getString(R.string.jedi_techniquesLong),
+            getString(R.string.jedi_techniquesAuthor), getString(R.string.jedi_techniquesRating)))
+        developingBook?.add(CardBook(R.drawable.speed_reading, getString(R.string.speed_reading), getString(R.string.speed_readingLong),
+            getString(R.string.speed_readingAuthor), getString(R.string.speed_readingRating)))
+        developingBook?.add(CardBook(R.drawable.strength_of_will, getString(R.string.strength_of_will), getString(R.string.strength_of_willLong),
+            getString(R.string.strength_of_willAuthor), getString(R.string.strength_of_willRating)))
 
-        adventureMovies?.add(CardBook(R.drawable.pirates, getString(R.string.pirates), getString(R.string.piratesLong),
-            getString(R.string.piratesActors), getString(R.string.piratesRating)))
-        adventureMovies?.add(CardBook(R.drawable.future, getString(R.string.future), getString(R.string.futureLong),
-            getString(R.string.futureActors), getString(R.string.futureRating)))
+        fictionBook?.add(CardBook(R.drawable.welcome_to_class, getString(R.string.welcome_to_class), getString(R.string.welcome_to_classLong),
+            getString(R.string.welcome_to_classAuthor), getString(R.string.welcome_to_classRating)))
+        fictionBook?.add(CardBook(R.drawable.instruction_for_the_brain, getString(R.string.instruction_for_the_brain), getString(R.string.instruction_for_the_brainLong),
+            getString(R.string.instruction_for_the_brainAuthor), getString(R.string.instruction_for_the_brainRating)))
 
-        thrillerMovies?.add(CardBook(R.drawable.underworld, getString(R.string.underworld), getString(R.string.underworldLong),
-            getString(R.string.underworldActors), getString(R.string.underworldRating)))
+        philosophyBook?.add(CardBook(R.drawable.man_in_search, getString(R.string.man_in_search), getString(R.string.man_in_searchLong),
+            getString(R.string.man_in_searchAuthor), getString(R.string.man_in_searchRating)))
 
         binding?.categoriesMovies?.layoutManager = LinearLayoutManager(this)
         genresAdapter = GenresAdapter(this, genresMovie!!)
